e21c8549c291bf4a929c931d9f3442cb39d94096
Nguyễn Trần Công Duy <48121572+metycat16@users.noreply.github.com>
2019-05-01 21:42:41 +0700

    Class FileManage
    This class is used to move the result image to the result folder.

---
+++ FileManage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TextDetection
+{
+    public class FileManage
+    {
+        private string filename;
+        private string sourcePath = @"opencv";
+        private string targetPath = @"result";
+
+        public FileManage(string filename)
+        {
+            this.filename = filename;
+        }
+
+        public void CopyToResult()
+        {
+            string sourceFile = Path.Combine(sourcePath, this.filename);
+            string destFile = Path.Combine(targetPath, this.filename);
+
+            File.Copy(sourceFile, destFile, true);
+        }
+        public void MoveToResult()
+        {
+            string sourceFile = Path.Combine(sourcePath, this.filename);
+            string destFile = Path.Combine(targetPath, this.filename);
+            if (!File.Exists(destFile)) File.Move(sourceFile, destFile);
+
+        }
+    }
+}
