cba2eac935581e4826cdfa1fb9e6bffb9bb78108
Nguyễn Trần Công Duy <48121572+metycat16@users.noreply.github.com>
2019-05-26 22:02:56 +0700

    Class callcmd (Update design pattern)
    This class is used to call Command Prompt and add a command to call python function into it.

--- callcmd.cs
+++ callcmd.cs
@@ -8,6 +8,8 @@ using System.IO;
 
 namespace TextDetection
 {
+
+
     public class Callcmd
     {
         private string filepath;
@@ -23,6 +25,173 @@ namespace TextDetection
         {
             return this.filename;
         }
+
+
+        public void callcommand(string command)
+        {
+            Process cmd = new Process();
+            cmd.StartInfo.FileName = "cmd.exe";
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.CreateNoWindow = true;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.Start();
+
+
+            cmd.StandardInput.WriteLine(cdopencv);
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.WriteLine(command);
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+            cmd.WaitForExit();
+        }
+        public abstract class Detection
+        {
+            // abstract method 'callTextDetection()' 
+            public abstract void callTextDetection(string python, string filepath);
+        }
+
+        public class ImageDeAdapter : Detection
+        {
+            public override void callTextDetection(string python, string filepath)
+            {
+                ImageDetection temp = new ImageDetection();
+                temp.callTextDetectionFromImage(python, filepath);
+            }
+        }
+
+        public class VideoDeAdapter : Detection
+        {
+            public override void callTextDetection(string python, string filepath)
+            {
+                VideoDetection temp = new VideoDetection();
+                temp.callTextDetectionFromVideo(python, filepath);
+            }
+        }
+
+        public class StreamDeAdapter : Detection
+        {
+            public override void callTextDetection(string python, string filepath)
+            {
+                StreamDetection temp = new StreamDetection();
+                temp.callTextDetectionFromStream(python, filepath);
+            }
+        }
+
+        public class ImageDetection
+        {
+            public void callTextDetectionFromImage(string python, string filepath)
+            {
+                python = "python text_detection.py --east frozen_east_text_detection.pb --image ";
+                python = python + "\"" + filepath + "\"";
+            }
+        }
+        public class VideoDetection
+        {
+            public void callTextDetectionFromVideo(string python, string filepath)
+            {
+                python = "python text_detection_video.py --east frozen_east_text_detection.pb -v ";
+                python = python + "\"" + filepath + "\"";
+            }
+        }
+        public class StreamDetection
+        {
+            public void callTextDetectionFromStream(string python, string filepath = "")
+            {
+                python = "python text_detection_video.py --east frozen_east_text_detection.pb ";
+                python = python + "\"" + filepath + "\"";
+            }
+
+        }
+
+        public Detection FindModeDec(string filetype, string filename)
+        {
+            if (filetype == "Image")
+            {
+                ImageDeAdapter mode = new ImageDeAdapter();
+                filename = "[d]-" + this.filename;
+                return mode;
+            }
+            else if (filetype == "Video")
+            {
+                VideoDeAdapter mode = new VideoDeAdapter();
+                filename = "[v]-" + this.filename;
+                return mode;
+            }
+            else if (filetype == "Stream")
+            {
+                StreamDeAdapter mode = new StreamDeAdapter();
+                filename = "Stream";
+                return mode;
+            }
+            else
+            {
+                StreamDeAdapter mode = new StreamDeAdapter();
+                return mode;
+            }
+
+        }
+
+        public abstract class Recognition
+        {
+            // abstract method 'callTextDetection()' 
+            public abstract void callTextRecognition(string python, string filepath);
+        }
+
+        public class ImageReAdapter : Recognition
+        {
+            public override void callTextRecognition(string python, string filepath)
+            {
+                ImageRecognition temp = new ImageRecognition();
+                temp.callTextRecognitionFromImage(python, filepath);
+            }
+        }
+        public class ImageRecognition
+        {
+            public void callTextRecognitionFromImage(string python, string filepath)
+            {
+                python = "python text_recognition.py --east frozen_east_text_detection.pb --image ";
+                python = python + "\"" + filepath + "\"";
+            }
+        }
+
+        public Recognition FindModeRec(string filetype, string filename)
+        {
+            if (filetype == "Image")
+            {
+                ImageReAdapter mode = new ImageReAdapter();
+                filename = "[r]-" + this.filename;
+                return mode;
+            }
+            else
+            {
+                ImageReAdapter mode = new ImageReAdapter();
+                return mode;
+            }
+
+        }
+
+        public string callpy(int mode,string typefile)
+        {
+            string filename_return = "";
+            string python = "";
+
+            if (mode == 0)
+            {
+                Detection callpython = FindModeDec(typefile, filename_return);
+                callpython.callTextDetection(python, filepath);
+            }
+            else if (mode == 1)
+            {
+                Recognition callpython = FindModeRec(typefile, filename_return);
+                callpython.callTextRecognition(python, filepath);
+            }
+
+            callcommand(python);
+            return filename_return;
+        }
+
+
 
         public string callpython(int mode) //0: text detection; 1: text recognition; 2 or other: text video detection
         {
@@ -47,7 +216,6 @@ namespace TextDetection
                 python = python + "\"" + filepath + "\"";
                 filename_return = "[v]-" + this.filename;
             }
-
             //open cmd
             Process cmd = new Process();
             cmd.StartInfo.FileName = "cmd.exe";
@@ -71,6 +239,7 @@ namespace TextDetection
 
 
             return filename_return;
+
         }
 
         public string calltextdetection()
@@ -106,6 +275,9 @@ namespace TextDetection
             //call python
             cmd.StandardInput.WriteLine(python);
             cmd.StandardInput.Flush();
+
+            string test = cmd.StandardOutput.ReadLine();
+
             cmd.StandardInput.Close();
             cmd.WaitForExit();
             //Console.WriteLine(cmd.StandardOutput.ReadToEnd());
